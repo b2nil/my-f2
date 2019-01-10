7e5c327819ff4b755c00bfee14428c914cb9a2c7
Ken Ho <gitea@fake.local>
2019-01-11 03:24:14 +0800

    feat: support alipay and wexin

--- src/core.js
+++ src/core.js
@@ -8,23 +8,62 @@ const EVENTS_MAP = {
   touchcancel: 'touchCancel'
 };
 
+function strLen(str) {
+  let len = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) > 0 && str.charCodeAt(i) < 128) {
+      len++;
+    } else {
+      len += 2;
+    }
+  }
+  return len;
+}
+
+// override measureText
+if (typeof wx !== 'undefined' && wx.getSystemInfo) {
+  Core.Util.measureText = function(text, font, ctx) {
+    if (!ctx) {
+      let fontSize = 12;
+      if (font) {
+        fontSize = parseInt(font.split(' ')[3], 10);
+      }
+      fontSize /= 2;
+      return {
+        width: strLen(text) * fontSize
+      };
+    }
+    ctx.font = font || '12px sans-serif';
+    return ctx.measureText(text);
+  };
+}
+
 // 为小程序封装事件机制
 Core.Util.addEventListener = function(source, type, listener) {
-  const context = source.getContext('2d');
-  type = EVENTS_MAP[type]; // 支付宝小程序事件名为驼峰结构
-  context.emitter.addListener(type, listener);
+  if (typeof my !== 'undefined' && my.getSystemInfo) {
+    const context = source.getContext('2d');
+    type = EVENTS_MAP[type]; // 支付宝小程序事件名为驼峰结构
+    context.emitter.addListener(type, listener);
+  } else {
+    source.addListener(type, listener);
+  }
 };
 
 Core.Util.removeEventListener = function(source, type, listener) {
-  const context = source.getContext('2d');
-  type = EVENTS_MAP[type]; // 支付宝小程序事件名为驼峰结构
-  context.emitter.removeListener(type, listener);
+  if (typeof my !== 'undefined' && my.getSystemInfo) {
+    const context = source.getContext('2d');
+    type = EVENTS_MAP[type]; // 支付宝小程序事件名为驼峰结构
+    context.emitter.removeListener(type, listener);
+  } else {
+    source.removeListener(type, listener);
+  }
 };
 
 Core.Util.createEvent = function(event, chart) {
-  if(event[0]){
-    event = event[0]
-  }  
+  if (event[0]) {
+    event = event[0];
+  }
+
   const type = event.type;
   let x = 0;
   let y = 0;
--- src/renderer.js
+++ src/renderer.js
@@ -4,6 +4,15 @@
  * @version 1.0.0
  */
 
+/**
+   * wxapp textAlign align 可选值为 left|center|right
+   * 标准canvas textAlign align 可选值为 left|center|right|start|end
+ */
+const TEXT_ALIGN_MAP = {
+  start: 'left',
+  end: 'right'
+};
+
 const CAPITALIZED_ATTRS_MAP = {
   fillStyle: 'FillStyle',
   fontSize: 'FontSize',
@@ -18,25 +27,52 @@ const CAPITALIZED_ATTRS_MAP = {
   textBaseline: 'TextBaseline'
 };
 
-class Renderer {
-  constructor(myCtx) {
+class WxRenderer extends require('wolfy87-eventemitter/EventEmitter.min.js') {
+  constructor(wxCtx) {
+    super();
     const self = this;
-    self.ctx = myCtx;
-    self.style = {}; // just mock
-    self._initContext(myCtx);
+    self.ctx = wxCtx;
+    self.style = {};
+    self._initContext(wxCtx);
   }
-
   getContext(type) {
     if (type === '2d') {
       return this.ctx;
     }
   }
+  _initContext(wxCtx) {
+    Object.keys(CAPITALIZED_ATTRS_MAP).map(key => {
+      Object.defineProperty(wxCtx, key, {
+        set(value) {
+          if (key === 'textAlign') {
+            value = TEXT_ALIGN_MAP[value] ? TEXT_ALIGN_MAP[value] : value;
+          }
+          const name = `set${CAPITALIZED_ATTRS_MAP[key]}`;
+          wxCtx[name](value);
+        }
+      });
+      return key;
+    });
+  }
+}
 
+class MyRenderer {
+  constructor(myCtx) {
+    const self = this;
+    self.ctx = myCtx;
+    self.style = {};
+    self._initContext(myCtx);
+  }
+  getContext(type) {
+    if (type === '2d') {
+      return this.ctx;
+    }
+  }
   _initContext(myCtx) {
     Object.keys(CAPITALIZED_ATTRS_MAP).map(key => {
       Object.defineProperty(myCtx, key, {
         set(value) {
-          const name = 'set' + CAPITALIZED_ATTRS_MAP[key];
+          const name = `set${CAPITALIZED_ATTRS_MAP[key]}`;
           myCtx[name](value);
         }
       });
@@ -45,4 +81,8 @@ class Renderer {
   }
 }
 
-module.exports = Renderer;
+if (typeof wx !== 'undefined' && wx.getSystemInfo) {
+  module.exports = WxRenderer;
+} else if (typeof my !== 'undefined' && my.getSystemInfo) {
+  module.exports = MyRenderer;
+}
